c8bdb20d20edb8ebf3fa752b1bd2ab4664e85a65
jonmellerby <jon.mellerby@gmail.com>
2023-11-29 12:54:20 +0100

    Add player movement and change CMake

--- main.cpp
+++ main.cpp
@@ -3,40 +3,67 @@
 #include <vector>
 
 const int WINDOW_HEIGHT = 800; //Lös detta på ett bättre sätt sen
-const int WINDOW_WIDTH = 1000; //Lös detta på ett bättre sätt sen
+const int WINDOW_WIDTH = 1200; //Lös detta på ett bättre sätt sen
 const int DISTANCE_TO_BORDER = 30;
-sf::Vector2f PLAYER_DIMENSIONS(50, 300);
+const int INITIAL_PLAYER_SPEED = 8;
+sf::Vector2f PLAYER_DIMENSIONS(25, 200);
+
+enum Keys {
+    UP, DOWN
+};
 
 class Player 
 {
-
 public:
+    int playerSpeed;
     sf::Vector2f pos;
     sf::RectangleShape body;
+    std::unordered_map<Keys, sf::Keyboard::Key> keyBindings;
 
-    Player(int defaultX) : 
+    Player(int defaultX, std::unordered_map<Keys, sf::Keyboard::Key> keyBindings) : 
     pos(defaultX, (WINDOW_HEIGHT / 2) - (PLAYER_DIMENSIONS.y / 2)),
+    keyBindings(keyBindings),
+    playerSpeed(INITIAL_PLAYER_SPEED),
     body()
     {
         body.setSize(PLAYER_DIMENSIONS);
         body.setPosition(pos);
     }
 
+    void handleMovement() {
+        if (sf::Keyboard::isKeyPressed(keyBindings[UP])) {
+            int newPos = body.getPosition().y - playerSpeed;
+            body.setPosition(pos.x, std::max(0, newPos));
+        }   
+        if (sf::Keyboard::isKeyPressed(keyBindings[DOWN])) {
+            int newPos = body.getPosition().y + playerSpeed;
+            body.setPosition(pos.x, std::min(WINDOW_HEIGHT - (int) PLAYER_DIMENSIONS.y, newPos));
+        }   
+    }
+
+
 };
+
 
 int main()
 {
+    std::unordered_map<Keys, sf::Keyboard::Key> pLeftBindings{
+        {Keys::UP, sf::Keyboard::Key::W},
+        {Keys::DOWN, sf::Keyboard::Key::S}
+    };
 
-    std::vector<Player> players;
-    Player pLeft(DISTANCE_TO_BORDER);
-    Player pRight(WINDOW_WIDTH - (DISTANCE_TO_BORDER + PLAYER_DIMENSIONS.x));
-    players.push_back(pLeft);
-    players.push_back(pRight);
+    std::unordered_map<Keys, sf::Keyboard::Key> pRightBindings{
+        {Keys::UP, sf::Keyboard::Key::Up},
+        {Keys::DOWN, sf::Keyboard::Key::Down}};
+
+    std::vector<Player*> players;
+    Player pLeft(DISTANCE_TO_BORDER, pLeftBindings);
+    Player pRight(WINDOW_WIDTH - (DISTANCE_TO_BORDER + PLAYER_DIMENSIONS.x), pRightBindings);
+    players.push_back(&pLeft);
+    players.push_back(&pRight);
 
     sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "SFML works!");
-    // sf::RectangleShape playerOne(sf::Vector2f(100, 300));
-    // playerOne.setFillColor(sf::Color::Blue);
-    // playerOne.setPosition(300, 300);
+    window.setFramerateLimit(60);
 
     while (window.isOpen())
     {
@@ -47,9 +74,11 @@ int main()
                 window.close();
         }
 
+
         window.clear();
-        for (auto& player : players) {
-            window.draw(player.body);
+        for (Player* player : players) {
+            player->handleMovement();
+            window.draw(player->body);
         }
         window.display();
     }
